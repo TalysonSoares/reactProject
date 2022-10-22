0673e208dd679676112a9bf3e98a8b45841fda8d
TalysonSoares <talysonsoares40@gmail.com>
2022-10-22 09:42:07 -0300

    criando função para pegar o valor total do carrinho

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import React from "react";
 
 export default function App () {
   const [items, setItems] = React.useState(0);
-  const add = () => {
+  const [amount, setAmout] = React.useState(0);
+
+  const add = (price) => {
     setItems(items + 1);
+    setAmout(amount + price)
   }
 
   return (
@@ -13,14 +16,25 @@ export default function App () {
       <h1 className="mt-3">
        Carrinho de compra
        <span className="text-primary ms-3"> {items} </span> 
+       <span className="text-success ms-3">R$ {amount} </span> 
       </h1>
 
       <hr />
 
       <div>
-        <span className="me-3">Nome do Produto</span>
+        <span className="me-3">Teclado</span>
 
-        <button className="btn btn-success btn-sm" onClick={add}>Adicionar</button>
+        <button className="btn btn-success btn-sm" onClick={() => add(90)}>
+          Adicionar
+        </button>
+      </div>
+      
+      <div>
+        <span className="me-3">Mouse</span>
+
+        <button className="btn btn-success btn-sm" onClick={() => add(32)}>
+          Adicionar
+        </button>
 
         {/* <Button onClick={add} cor="success">
           Adcionar ao Carrinho
