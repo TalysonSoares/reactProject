1df597c6a74808d2ff3b0723350ee8f9bc0fb05c
TalysonSoares <talysonsoares40@gmail.com>
2022-10-22 09:22:34 -0300

    criando função para adicionar +1 ao cliclar no botão

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Button from "./components/Button";
 import React from "react";
 
 export default function App () {
-  const [items, setItems]
+  const [items, setItems] = React.useState(0);
+  const add = () => {
+    setItems(items + 1);
+  }
+
   return (
     <div className="container">
       <h1 className="mt-3">
        Carrinho de compra
-       <span className="text-primary ms-3">0</span> 
+       <span className="text-primary ms-3"> {items} </span> 
       </h1>
 
       <hr />
@@ -16,7 +20,7 @@ export default function App () {
       <div>
         <span className="me-3">Nome do Produto</span>
 
-        <Button cor="success">
+        <Button onClick={add} cor="success">
           Adcionar ao Carrinho
         </Button>
       </div>
